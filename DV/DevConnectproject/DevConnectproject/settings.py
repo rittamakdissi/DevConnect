9f8bb678adb527b7dca3559b976b2ef02b5f2b3b
rittamakdissi <rittamakdissi@gmail.com>
2026-04-09 12:28:51 +0300

    تحسينات 2

--- DV/DevConnectproject/DevConnectproject/settings.py
+++ DV/DevConnectproject/DevConnectproject/settings.py
@@ -166,8 +166,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
