fd1803f2d4a345b795f0cb54418b612db9dd58df
rittamakdissi <rittamakdissi@gmail.com>
2026-04-15 16:09:14 +0300

    static files

--- DV/DevConnectproject/DevConnectproject/settings.py
+++ DV/DevConnectproject/DevConnectproject/settings.py
@@ -161,12 +161,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
